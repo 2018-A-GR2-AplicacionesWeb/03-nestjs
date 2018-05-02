cea6177f7b2c148b98876bfb06275c23737cb1c9
Adrian Eguez <eadepto@hotmail.com>
2018-05-02 15:48:01 -0500

    anadir arreglo no quemado de peliculas y metodo para obtener parameotr sde ruta

--- project2/src/app.pelicula-controller.ts
+++ project2/src/app.pelicula-controller.ts
@@ -1,10 +1,30 @@
-import {Controller, Get} from "@nestjs/common";
+import {Controller, Get, HttpCode, Param, Post} from "@nestjs/common";
 
 @Controller('Pelicula')
 export class AppPeliculaController {
+    peliculas: Pelicula[] = [];
 
     @Get('mostrarCartelera')
     mostrarCartelera() {
-        return ['Thor', 'Capi', 'Spidey'];
+        return this.peliculas;
+    }
+
+    @Post('mostrarCartelera/:nombre/:estreno')
+    @HttpCode(203)
+    anadirACartelera(@Param() parametros: Pelicula) {
+
+        this.peliculas
+            .push(new Pelicula(
+                parametros.nombre,
+                parametros.estreno)
+            );
+
+        return this.peliculas;
+    }
+}
+
+class Pelicula {
+    constructor(public nombre?: string,
+                public estreno?: number) {
     }
 }
