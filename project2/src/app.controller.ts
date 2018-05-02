9ddd791973e16c8309d53367f88d48448cdf77e4
Adrian Eguez <eadepto@hotmail.com>
2018-05-02 15:17:40 -0500

    aplicaciones tradicionales

--- project2/src/app.controller.ts
+++ project2/src/app.controller.ts
@@ -1,9 +1,19 @@
-import { Get, Controller } from '@nestjs/common';
+import {Get, Controller} from '@nestjs/common';
 
+const fs = require('fs');
+let contador = 0;
 @Controller()
 export class AppController {
-  @Get()
-  root(): string {
-    return 'Hello World!';
-  }
+    @Get()
+    root() {
+        console.log('Entro al metodo');
+        contador++;
+        let datosArchivo;
+        let html = fs.readFileSync(
+            __dirname + '/html/Index.html',
+            'utf8'
+        );
+        html = html.replace('{{variable}}',contador);
+        return html; // contenido o un error
+    }
 }
