35b8b72b4e5a2dfdc05f1f8519e09f41f92b5927
Adrian Eguez <eadepto@hotmail.com>
2018-05-09 14:37:23 -0500

    parametros

--- project2/src/parametros.controller.ts
+++ project2/src/parametros.controller.ts
@@ -1,10 +1,28 @@
-import {Body, Controller, Param, Post, Query, Req, Res} from "@nestjs/common";
+import {Body, Controller, Get, Param, Post, Query, Req, Res} from "@nestjs/common";
 
 @Controller('Parametros')
 export class ParametrosController {
 
-    @Post('recuperar/:id')
+    @Post('recuperar/:id/:materia')
     recuperarParametros(
+        @Req() request,
+        @Res() response,
+        @Param() paramParams,
+        @Query() queryParams,
+        @Body() bodyParams
+    ) {
+        const respuesta = {
+            paramParams: paramParams,
+            queryParams: queryParams,
+            bodyParams: bodyParams,
+        };
+        console.log(respuesta);
+        return response.send(respuesta)
+
+    }
+
+    @Get('recuperar/:id/:materia')
+    recuperarParametrosConGet(
         @Req() request,
         @Res() response,
         @Param() paramParams,
