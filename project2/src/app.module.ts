6b9c807c0d73833b0f76c0df0351d1c961bbc9a4
Adrian Eguez <eadepto@hotmail.com>
2018-05-15 17:54:50 -0500

    typeorm

--- project2/src/app.module.ts
+++ project2/src/app.module.ts
@@ -5,41 +5,33 @@ import {ParametrosController} from "./parametros.controller";
 import {UsuarioService} from "./usuario.service";
 import {LogMiddleware} from "./log.middleware";
 import {UsuarioController} from "./usuario.controller";
-
-import { TypeOrmModule } from '@nestjs/typeorm';
+import {UsuarioModule} from "./usuario/usuario.module";
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: '172.31.104.77',
-            port: 3306,
-            username: 'root',
-            password: 'root',
-            database: 'web',
-            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-            synchronize: true,
-        }),
+        TypeOrmModule
+            .forRoot({
+                type: 'mysql',
+                host: '172.31.104.77',
+                port: 3306,
+                username: 'root',
+                password: 'root',
+                database: 'web',
+                entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+                synchronize: true,
+            }),
+        // UsuarioModule,
     ], // Importar otros modulos
     controllers: [
-        AppController,
-        AppPeliculaController,
-        ParametrosController,
-        UsuarioController
+        // AppController,
+        // AppPeliculaController,
+        // ParametrosController,
+        // UsuarioController
     ],
     components: [
-        UsuarioService
+        // UsuarioService
     ], // Componentes
 })
-export class AppModule implements NestModule {
-    configure(consumer: MiddlewaresConsumer) {
-        consumer
-            .apply(LogMiddleware)
-            .with('EPN', 1989)
-            .forRoutes(
-                ParametrosController,
-                AppPeliculaController,
-                AppController
-            );
-    }
+export class AppModule{
 }
