ab1e9e2e9ba408dc5c8ead1c04bba2d5de30f12b
Adrian Eguez <eadepto@hotmail.com>
2018-05-23 14:54:10 -0500

    reflector

--- NEST5/src/guards/usuario.guard.ts
+++ NEST5/src/guards/usuario.guard.ts
@@ -2,13 +2,18 @@ import {CanActivate, ExecutionContext, Injectable} from "@nestjs/common";
 import {Observable} from "rxjs/index";
 
 @Injectable()
-export class UsuarioGuard implements CanActivate{
+export class UsuarioGuard implements CanActivate {
 
     canActivate(
         context: ExecutionContext):
         boolean |
         Promise<boolean> |
         Observable<boolean> {
+        const request = context
+            .switchToHttp()
+            .getRequest();
+        console.log('Request', request);
+        console.log('Cabeceras', request.headers);
 
         return false;
 
--- NEST5/src/usuario.controller.ts
+++ NEST5/src/usuario.controller.ts
@@ -1,11 +1,18 @@
-import {Controller, Get} from "@nestjs/common";
+import {Controller, Get, ReflectMetadata, UseGuards} from "@nestjs/common";
+import {UsuarioGuard} from "./guards/usuario.guard";
 
 @Controller('usuario')
+@UseGuards(UsuarioGuard)
 export class UsuarioController {
+
     @Get('mostrar')
+    @ReflectMetadata('nombreDato','ValorM')
     mostrar() {
+        return 'Ok mostrar';
     }
     @Get('crear')
+    @ReflectMetadata('nombreDato','ValorC')
     crear() {
+        return 'Ok crear';
     }
 }
